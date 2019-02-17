19fb747894457f240296b571afc292b2a406aac9
adenjimmy <nbhnazfar@gmail.com>
2019-02-17 22:46:01 +0700

    First Commit

---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="home">
+        <div class="row">
+            <img class="img-responsive imgtron" src="static/images/Home-Screen-Web.jpg">
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'home'
+    }
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: '/',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    }
+  ]
+})
